95bb63547e0797a0f837ef70dd288fde7ffafeb5
Muhammad Fahmi Irfananda <fahmiirfan909@gmail.com>
2018-11-09 00:40:45 +0700

    add unit test in FavMatchPresenter

--- app/src/main/java/io/github/miun173/footballfans/main/MainActivity.kt
+++ app/src/main/java/io/github/miun173/footballfans/main/MainActivity.kt
@@ -1,7 +1,7 @@
 package io.github.miun173.footballfans.main
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import io.github.miun173.footballfans.R
 import io.github.miun173.footballfans.main.favmatch.FavmatchFragment
 import io.github.miun173.footballfans.main.schedule.ScheduleFragment
---
+++ app/src/test/java/io/github/miun173/footballfans/main/schedule/SchedulePresenterTest.kt
@@ -0,0 +1,15 @@
+package io.github.miun173.footballfans.main.schedule
+
+import org.junit.Before
+import org.junit.Test
+
+class SchedulePresenterTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun getEvents() {
+    }
+}
